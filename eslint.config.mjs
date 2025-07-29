ee04b975a558df60ece6318acf73a1760fe15d81
gianpietrodimatteo <gianpietro.dimatteo@gmail.com>
2025-07-29 12:10:22 -0300

    Added cursorrules Also added prettier, adjusted project folder structure to /src/ Added prisma's first basic scheme

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -11,6 +11,11 @@ const compat = new FlatCompat({
 
 const eslintConfig = [
   ...compat.extends("next/core-web-vitals", "next/typescript"),
+  {
+    settings: {
+      maxWarnings: 0
+    }
+  }
 ];
 
 export default eslintConfig;
